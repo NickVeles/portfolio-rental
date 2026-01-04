7b12b3726100884ef3770d499cda9fb415690d3a
NickVeles <xm1k123x@gmail.com>
2026-01-04 16:28:58 +0100

    feat: add FeaturesSection

--- app/(nondashboard)/landing/page.tsx
+++ app/(nondashboard)/landing/page.tsx
@@ -1,8 +1,13 @@
-import React from "react";
 import HeroSection from "./HeroSection";
+import FeaturesSection from "./FeaturesSection";
 
 const Landing = () => {
-  return <div><HeroSection /></div>;
+  return (
+    <div>
+      <HeroSection />
+      <FeaturesSection />
+    </div>
+  );
 };
 
 export default Landing;
