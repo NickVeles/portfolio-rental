3f5edfee494697f41bc426fefc801e0afd10108c
NickVeles <xm1k123x@gmail.com>
2026-01-04 17:06:48 +0100

    feat: add CTA section

--- app/(nondashboard)/landing/page.tsx
+++ app/(nondashboard)/landing/page.tsx
@@ -1,6 +1,7 @@
 import HeroSection from "./HeroSection";
 import FeaturesSection from "./FeaturesSection";
 import DiscoverSection from "./DiscoverSection";
+import CallToActionSection from "./CallToActionSection";
 
 const Landing = () => {
   return (
@@ -8,6 +9,7 @@ const Landing = () => {
       <HeroSection />
       <FeaturesSection />
       <DiscoverSection />
+      <CallToActionSection />
     </div>
   );
 };
