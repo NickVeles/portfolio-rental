779bba36a612664c31d139a28e79976bab16c753
NickVeles <xm1k123x@gmail.com>
2026-01-04 15:14:50 +0100

    refactor: turn logo file into react component

---
+++ components/Icons.tsx
@@ -0,0 +1,17 @@
+import { SVGProps } from "react";
+
+export const Logo = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="46px"
+    height="44px"
+    viewBox="0 0 46 44"
+    fill="none"
+    {...props}
+  >
+    <path
+      fill="currentColor"
+      d="M45.8 12.5V0c-8 0-15.8 2.7-21.4 7.4l-1.5 1.2-1.5-1.2C15.7 2.7 8 0 0 0v12.5c0 3.4 1.2 6.6 3.2 9.3-2 2.7-3.2 5.9-3.2 9.3v12.5c8 0 15.8-2.7 21.4-7.4l1.5-1.2c-3.9-2.3-6.3-6.4-6.3-11v-4.4c0-4.5 2.4-8.7 6.3-11 3.9 2.3 6.3 6.5 6.3 11V24c0 4.5-2.4 8.7-6.3 11l1.5 1.2c5.7 4.7 13.4 7.4 21.4 7.4V31.1c0-3.4-1.2-6.6-3.2-9.3 2-2.7 3.2-5.9 3.2-9.3Z"
+    />
+  </svg>
+);
