750f9c52f2caf6b379e355059350e0a5a4c0414f
NickVeles <xm1k123x@gmail.com>
2026-01-07 13:01:39 +0100

    feat: update logo

--- client/components/Icons.tsx
+++ client/components/Icons.tsx
@@ -3,15 +3,23 @@ import { SVGProps } from "react";
 export const Logo = (props: SVGProps<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="46px"
-    height="44px"
-    viewBox="0 0 46 44"
-    fill="none"
+    width={32}
+    height={32}
+    viewBox="0 0 8.467 8.467"
     {...props}
   >
     <path
-      fill="currentColor"
-      d="M45.8 12.5V0c-8 0-15.8 2.7-21.4 7.4l-1.5 1.2-1.5-1.2C15.7 2.7 8 0 0 0v12.5c0 3.4 1.2 6.6 3.2 9.3-2 2.7-3.2 5.9-3.2 9.3v12.5c8 0 15.8-2.7 21.4-7.4l1.5-1.2c-3.9-2.3-6.3-6.4-6.3-11v-4.4c0-4.5 2.4-8.7 6.3-11 3.9 2.3 6.3 6.5 6.3 11V24c0 4.5-2.4 8.7-6.3 11l1.5 1.2c5.7 4.7 13.4 7.4 21.4 7.4V31.1c0-3.4-1.2-6.6-3.2-9.3 2-2.7 3.2-5.9 3.2-9.3Z"
+      d="M4.233.53.53 4.232h1.323l2.381-2.38 2.382 2.38h1.322L4.233.53zm3.704 3.703L4.233 7.937h3.704V4.233zM4.233 7.937.53 4.233v3.704h3.704zm2.382-3.704H5.292L4.233 3.175 3.175 4.233H1.852l2.381 2.382 2.382-2.382z"
+      style={{
+        fill: "currentColor",
+        strokeWidth: 0.79374999,
+        strokeMiterlimit: 4.5,
+        stroke: "none",
+        strokeOpacity: 1,
+        strokeDasharray: "none",
+        strokeLinejoin: "bevel",
+        paintOrder: "stroke fill markers",
+      }}
     />
   </svg>
 );
