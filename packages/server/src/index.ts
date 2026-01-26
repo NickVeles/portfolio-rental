fad36fea034fb32ccbd025f19894b24aa192e48d
NickVeles <xm1k123x@gmail.com>
2026-01-26 18:25:23 +0100

    feat: add tenant routes

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -4,8 +4,10 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import { authMiddleware } from './middleware/authMiddleware.js';
 
 /* ROUTE IMPORT */
+import tenantRoutes from "./routes/tenantRoutes";
 
 /* CONFIGURATIONS */
 dotenv.config();
@@ -22,6 +24,8 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('This is home route');
 })
+
+app.use("/tenants", authMiddleware(["tenant"]), tenantRoutes);
 
 /* SERVER */
 const port = process.env.PORT || 3002;
