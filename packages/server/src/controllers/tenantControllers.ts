cfe8e79e2a0ec97ba7f76a1468086ee8e773bfbb
NickVeles <xm1k123x@gmail.com>
2026-01-26 18:24:29 +0100

    feat: add tenantControllers

---
+++ packages/server/src/controllers/tenantControllers.ts
@@ -0,0 +1,61 @@
+import type { Request, Response } from "express";
+import { PrismaClient } from "@portfolio-rental/shared";
+
+const prisma = new PrismaClient();
+
+export const getTenant = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { cognitoId } = req.params;
+
+    if (typeof cognitoId !== "string") {
+      res.status(400).json({ message: "Invalid cognitoId" });
+      return;
+    }
+
+    const tenant = await prisma.tenant.findUnique({
+      where: { cognitoId },
+      include: {
+        favorites: true,
+      },
+    });
+
+    if (tenant) {
+      res.json(tenant);
+    } else {
+      res.status(404).json({ message: "Tenant not found" });
+    }
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving tenant: ${error.message}` });
+  }
+};
+
+export const createTenant = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { cognitoId, name, email, phoneNumber } = req.body;
+
+    if (typeof cognitoId !== "string") {
+      res.status(400).json({ message: "Invalid cognitoId" });
+      return;
+    }
+
+    const tenant = await prisma.tenant.create({
+      data: {
+        cognitoId,
+        name,
+        email,
+        phoneNumber,
+      },
+    });
+
+    res.status(201).json(tenant);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error creating tenant: ${error.message}` });
+  }
+};
