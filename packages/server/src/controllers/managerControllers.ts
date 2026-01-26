f292cfb5ca29a99bfba21598651afc3a68075df1
NickVeles <xm1k123x@gmail.com>
2026-01-26 18:29:51 +0100

    feat: add manager controllers

---
+++ packages/server/src/controllers/managerControllers.ts
@@ -0,0 +1,61 @@
+import type { Request, Response } from "express";
+import { PrismaClient } from "@portfolio-rental/shared";
+
+const prisma = new PrismaClient();
+
+export const getManager = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { cognitoId } = req.params;
+
+    if (typeof cognitoId !== "string") {
+      res.status(400).json({ message: "Invalid cognitoId" });
+      return;
+    }
+
+    const manager = await prisma.manager.findUnique({
+      where: { cognitoId },
+    });
+
+    if (manager) {
+      res.json(manager);
+    } else {
+      res.status(404).json({ message: "Manager not found" });
+    }
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error retrieving manager: ${error.message}` });
+  }
+};
+
+export const createManager = async (
+  req: Request,
+  res: Response,
+): Promise<void> => {
+  try {
+    const { cognitoId, name, email, phoneNumber } = req.body;
+
+    if (typeof cognitoId !== "string") {
+      res.status(400).json({ message: "Invalid cognitoId" });
+      return;
+    }
+
+    const manager = await prisma.manager.create({
+      data: {
+        cognitoId,
+        name,
+        email,
+        phoneNumber,
+      },
+    });
+
+    res.status(201).json(manager);
+  } catch (error: any) {
+    res
+      .status(500)
+      .json({ message: `Error creating manager: ${error.message}` });
+  }
+};
