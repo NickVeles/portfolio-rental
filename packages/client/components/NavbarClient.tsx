66a89d98fe55c20fc2e23547616d14bd6610b62d
NickVeles <xm1k123x@gmail.com>
2026-01-27 20:49:00 +0100

    fix: improve sidebar trigger for mobile view in navbar

--- packages/client/components/NavbarClient.tsx
+++ packages/client/components/NavbarClient.tsx
@@ -47,7 +47,7 @@ function NavbarClient({ initialUser }: NavbarClientProps) {
   return (
     <div className="fixed top-0 left-0 w-full z-45 shadow-xl">
       <div
-        className="flex justify-between items-center w-full py-3 px-8 bg-primary-700 text-background"
+        className="flex justify-between items-center w-full py-3 px-4 md:px-8 bg-primary-700 text-background"
         style={{ minHeight: NAVBAR_HEIGHT }}
       >
         <div className="flex items-center gap-4 md:gap-6">
