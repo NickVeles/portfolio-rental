58835926d3788c80a25b46aa3234f5946e1d41c1
NickVeles <xm1k123x@gmail.com>
2026-01-27 20:15:57 +0100

    fix: add missing sidebar provider

--- packages/client/app/providers.tsx
+++ packages/client/app/providers.tsx
@@ -3,12 +3,15 @@
 import { Authenticator } from "@aws-amplify/ui-react";
 import StoreProvider from "../state/redux";
 import Auth from "./(auth)/authProvider";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
       <Authenticator.Provider>
-        <Auth>{children}</Auth>
+        <Auth>
+          <SidebarProvider>{children}</SidebarProvider>
+        </Auth>
       </Authenticator.Provider>
     </StoreProvider>
   );
