0f22d18325a81fee0f435252ccba581bc8ec0bf5
NickVeles <xm1k123x@gmail.com>
2026-02-28 13:01:56 +0100

    fix: correct import path for lease controllers in lease routes

--- packages/server/src/routes/leaseRoutes.ts
+++ packages/server/src/routes/leaseRoutes.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { authMiddleware } from "../middleware/authMiddleware";
-import { getLeasePayments, getLeases } from "../controllers/leaseController";
+import { getLeasePayments, getLeases } from "../controllers/leaseControllers";
 
 const leaseRoutes = express.Router();
 
